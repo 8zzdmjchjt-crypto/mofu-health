d4603750df117a4152a48350535ab1bf451f0215
8zzdmjchjt-crypto <8zzdmjchjt@privaterelay.appleid.com>
2026-06-15 10:50:31 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mofunote-v183-calendar-selectedfix';
+const CACHE='mofunote-v184-profilepadding';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.png','./animal_chinchilla.png','./animal_degu.png','./animal_guineapig.png','./animal_hamster.png','./animal_rabbit.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
